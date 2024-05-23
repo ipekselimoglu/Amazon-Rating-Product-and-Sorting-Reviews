53df2d6ec4b3cb07967202ed824441e5c8619f42
İPEK SELİMOĞLU <36934196+ipekselimoglu@users.noreply.github.com>
2024-05-23 12:57:32 +0300

    Add files via upload

---
+++ Rating Product & Sorting Reviews in Amazon.py
@@ -0,0 +1,116 @@
+###################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+# İş Problemi
+###################################################
+
+# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
+# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
+# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
+# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
+# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
+# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID - ID of the reviewer, e.g. A2SUAM1J3GNN3B
+# asin - ID of the product, e.g. 0000013714
+# reviewerName - name of the reviewer
+# helpful - helpfulness rating of the review, e.g. 2/3
+# reviewText - text of the review
+# overall - rating of the product
+# summary - summary of the review
+# unixReviewTime - time of the review (unix time)
+# reviewTime - time of the review (raw)
+# day_diff - Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes - Değerlendirmenin faydalı bulunma sayısı
+# total_vote - Değerlendirmeye verilen oy sayısı
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import scipy.stats as st
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+df = pd.read_csv("datasets/amazon_review.csv")
+df["overall"].mean()  # Ürünün Ortalama Puanı
+
+
+# Tarihe Göre Ağırlıklı Puan Ortalaması
+# zaman bazlı ortalama ağırlıkların belirlenmesi
+def time_based_weighted_average(dataframe, w1=50, w2=25, w3=15, w4=10):
+    return dataframe.loc[dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.25), "overall"].mean() * w1 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.25)) & (
+                    dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.50)), "overall"].mean() * w2 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.50)) & (
+                    dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.75)), "overall"].mean() * w3 / 100 + \
+        dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.75)), "overall"].mean() * w4 / 100
+
+
+# helpful_no Değişkenini Üretilidi
+df["helpful_no"] = df["total_vote"] - df["helpful_yes"]
+df = df[["reviewerName", "overall", "summary", "helpful_yes", "helpful_no", "total_vote", "reviewTime"]]
+
+
+def wilson_lower_bound(up, down, confidence=0.95):
+    """
+    Wilson Lower Bound Score hesapla
+
+    - Bernoulli parametresi p için hesaplanacak güven aralığının alt sınırı WLB skoru olarak kabul edilir.
+    - Hesaplanacak skor ürün sıralaması için kullanılır.
+    - Not:
+    Eğer skorlar 1-5 arasıdaysa 1-3 negatif, 4-5 pozitif olarak işaretlenir ve bernoulli'ye uygun hale getirilebilir.
+    Bu beraberinde bazı problemleri de getirir. Bu sebeple bayesian average rating yapmak gerekir.
+
+    Parameters
+    ----------
+    up: int
+        up count
+    down: int
+        down count
+    confidence: float
+        confidence
+
+    Returns
+    -------
+    wilson score: float
+
+    """
+    n = up + down
+    if n == 0:
+        return 0
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    phat = 1.0 * up / n
+    return (phat + z * z / (2 * n) - z * math.sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+
+
+def score_up_down_diff(up, down):
+    return up - down
+
+
+def score_average_rating(up, down):
+    if up + down == 0:
+        return 0
+    return up / (up + down)
+
+
+# score_pos_neg_diff
+df["score_pos_neg_diff"] = df.apply(lambda x: score_up_down_diff(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+# score_average_rating
+df["score_average_rating"] = df.apply(lambda x: score_average_rating(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+# wilson_lower_bound
+df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+df.sort_values("wilson_lower_bound", ascending=False).head(20)
